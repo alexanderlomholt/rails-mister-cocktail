95704a86f437109f9b4f59b7ef5696059d32e868
Alexander Lomholt <arlomholt@gmail.com>
2018-02-22 13:57:30 -0500

    models, controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: [ :index, :new, :create, :show ] do
+    resources :doses, except: [ :destroy ] do
+      resources :ingredients
+    end
+  end
+
+  resources :doses, only: [ :destroy ]
+
+  root "cocktails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
